9ede2e5cfe411f461728156b47e4cabcf462e0d3
맹순영 <symaeng98@naver.com>
2023-06-08 00:26:46 +0900

    fix: 로딩 화면 변경

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -9,7 +9,7 @@ function Loading(props) {
     return (
         <div className={styles["loading"]}>
             <div>
-                <img src="/loading.gif" alt="loading"/>
+                <img src="/pacman.gif" alt="loading"/>
             </div>
             <p>교안 양에 따라 최대 1분 정도 소요될 수 있습니다</p>
         </div>
